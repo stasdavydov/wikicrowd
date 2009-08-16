5a057e0a650dba0881e5010a54841cd234d9a8f5
VolkVolkVolkVolk <VolkVolkVolkVolk@3d7cdbb2-4fc4-11de-8087-4121bd74a06b>
2009-08-16 22:28:29 +0000

    Introduce unit-testing with SimpleTest environment.

--- test/wiki_syntax_test.php
+++ test/wiki_syntax_test.php
@@ -2,19 +2,29 @@
 
 // todo: create unit-test for wiki-syntax
 
-$str = '*bold*';
-$expected = '<strong>bold</strong>';
+require_once('simpletest/autorun.php');
 
-$str = '/italic/';
-$expected = '<em>italic</em>';
+class TestWikiSyntax extends UnitTestCase {
+	function testBold1() {
+		$str = '*bold*';
+		$expected = '<strong>bold</strong>';
+	}
 
+	function testItalic1() {
+		$str = '/italic/';
+		$expected = '<em>italic</em>';
+	}
 
-$str = '_subscript_';
-$expected = '<sub>subscript</sub>';
+	function testSubscript1() {
+		$str = '_subscript_';
+		$expected = '<sub>subscript</sub>';
+	}
 
-
-$str = '^superscript^';
-$expected = '<sup>superscript</sup>';
+	function testSuperScript1() {
+		$str = '^superscript^';
+		$expected = '<sup>superscript</sup>';
+	}
+}
 
 // todo: add more cases
 
